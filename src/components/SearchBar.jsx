6f0f7aea19ef3d693051b94c2f4a4e5b122b4a21
Pushpendra Kukreti <41129279+pushpendrakukreti@users.noreply.github.com>
2023-04-02 00:43:02 +0530

    chore:adding separate search bar component, feature to set list null when click outside search bar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,132 @@
+import React, { useEffect, useRef, useState } from "react";
+import "../scenes/navbar/styles.css";
+import {
+    useTheme,
+    useMediaQuery,
+    Icon,
+} from "@mui/material";
+import {
+    Clear,
+} from "@mui/icons-material";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "components/FlexBetween";
+import List from "@mui/material/List";
+import ListItemText from "@mui/material/ListItemText";
+import Debounce from "./Debounce";
+import FriendRequest from "components/FriendRequest";
+
+const SearchBar = (prop) => {
+    const [searchUserList, setSearchUserList] = useState([]);
+    const navigate = useNavigate();
+    const user = useSelector((state) => state.user);
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+    const theme = useTheme();
+    const neutralLight = theme.palette.neutral.light;
+    const token = useSelector((state) => state.token);
+    // const debounceGetSearch = Debounce(getSearchResult, 500);
+    const useSearchStarwarsHero = () => Debounce((text) => getSearchResult(text));
+    const { inputText, setInputText } = useSearchStarwarsHero();
+    const refClick = useRef(null);
+
+    /*Outside search box click to set search list null */
+    const handleClickOutside = (e) => {
+        if (!refClick.current.contains(e.target)) {
+            setInputText("");
+            setSearchUserList([]);
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener("click", handleClickOutside, true)
+    }, [])
+
+    const routeChange = (userId) => {
+        let path = `/profile/${userId}`;
+        navigate(path);
+        navigate(0);
+    };
+
+    const onKeyDown = (e) => {
+        if (e.keyCode === 8 && e.currentTarget.value.length <= 1) {
+            setSearchUserList([]);
+            setInputText(e.keyCode);
+        }
+    };
+
+    const getSearchResult = async (values) => {
+        let data = [];
+        if (values.length > 0 && values !== 8) {
+            const savedUserResponse = await fetch(
+                `http://localhost:3001/users/${values}/searchUser`,
+                {
+                    method: "GET",
+                    headers: { Authorization: `Bearer ${token}` },
+                }
+            );
+
+            try {
+                data = await savedUserResponse.json();
+            }
+            catch (err) {
+                console.log(err);
+            }
+        }
+
+        data.length > 0 ? setSearchUserList([...data]) : setSearchUserList([]);
+    };
+
+    return (
+        <div ref={refClick}>
+            {isNonMobileScreens && (
+                <FlexBetween
+                    backgroundColor={neutralLight}
+                    borderRadius="9px"
+                    gap="2rem"
+                    padding="0.1rem 1rem"
+                    marginTop="1.7vh"
+                >
+                    <input
+                        className="MuiInputBase-input css-yz9k0d-MuiInputBase-input input-name"
+                        placeholder="Search..."
+                        type="text"
+                        value={inputText.length > 0 ? inputText : ""}
+                        onChange={(e) => setInputText(e.currentTarget.value)}
+                        onKeyDown={(e) => onKeyDown(e)}
+                        autoFocus />
+                    <Icon>
+                        {
+                            inputText.length > 0 &&
+                            <Clear
+                                className="clear-search"
+                                onClick={() => (setInputText(""), setSearchUserList([]))}
+                            ></Clear>
+                        }
+                    </Icon>
+                </FlexBetween>
+            )}
+            <List className={searchUserList.length > 0 ? "search-box-container" : ""}>
+                {searchUserList &&
+                    searchUserList?.map(
+                        ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+                            return (
+                                <li key={_id}>
+                                    <button className="btn-list primary" key={_id + 1} onClick={() => routeChange(_id)}>
+                                        <img src={('http://localhost:3001/assets/' + picturePath)} className="img-profile" />
+                                        <ListItemText
+                                            className="lst-items"
+                                            primary={`${firstName} ${lastName}`}
+                                            secondary={`${occupation}-${location}`}
+                                        />
+                                    </button>
+                                    {(user._id !== _id) && <FriendRequest friendId={_id} />}
+                                </li>
+                            );
+                        }
+                    )}
+            </List>
+        </div>
+    )
+}
+
+export default SearchBar;
--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -10,12 +10,9 @@ import {
   FormControl,
   useTheme,
   useMediaQuery,
-  FormGroup,
   Button,
-  Icon,
 } from "@mui/material";
 import {
-  Search,
   Message,
   DarkMode,
   LightMode,
@@ -23,22 +20,15 @@ import {
   Help,
   Menu,
   Close,
-  Delete,
-  Clear,
 } from "@mui/icons-material";
 import { useDispatch, useSelector } from "react-redux";
 import { setMode, setLogout } from "state";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import FlexBetween from "components/FlexBetween";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import Debounce from "../../components/Debounce";
-import FriendRequest from "components/FriendRequest";
+import SearchBar from "../../components/SearchBar";
 
 const Navbar = () => {
   const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
-  const [searchUserList, setSearchUserList] = useState([]);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((state) => state.user);
@@ -48,55 +38,14 @@ const Navbar = () => {
   const neutralLight = theme.palette.neutral.light;
   const dark = theme.palette.neutral.dark;
   const background = theme.palette.background.default;
-  const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
   const fullName = `${user.firstName} ${user.lastName}`;
-  const token = useSelector((state) => state.token);
 
   const navigateHome = () => {
     // 👇️ navigate to /
     navigate('/home');
   };
-
-  const routeChange = (userId) => {
-    let path = `/profile/${userId}`;
-    navigate(path);
-    navigate(0);
-  };
-
-  const onKeyDown = (e) => {
-    if (e.keyCode === 8 && e.currentTarget.value.length <= 1) {
-      setSearchUserList([]);
-      setInputText(e.keyCode);
-    }
-  };
-
-  const getSearchResult = async (values) => {
-    let data = [];
-    if (values.length > 0 && values !== 8) {
-      const savedUserResponse = await fetch(
-        `http://localhost:3001/users/${values}/searchUser`,
-        {
-          method: "GET",
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      );
-
-      try {
-        data = await savedUserResponse.json();
-      }
-      catch (err) {
-        console.log(err);
-      }
-    }
-
-    data.length > 0 ? setSearchUserList([...data]) : setSearchUserList([]);
-  };
-
-  // const debounceGetSearch = Debounce(getSearchResult, 500);
-  const useSearchStarwarsHero = () => Debounce((text) => getSearchResult(text));
-  const { inputText, setInputText } = useSearchStarwarsHero();
 
   //setSearch
   return (
@@ -112,58 +61,7 @@ const Navbar = () => {
           ></i>
         </Button>
 
-        <div>
-          {isNonMobileScreens && (
-            <FlexBetween
-              backgroundColor={neutralLight}
-              borderRadius="9px"
-              gap="2rem"
-              padding="0.1rem 1rem"
-              marginTop="1.7vh"
-            >
-              <input
-                className="MuiInputBase-input css-yz9k0d-MuiInputBase-input input-name"
-                placeholder="Search..."
-                type="text"
-                value={inputText.length > 0 ? inputText : ""}
-                onChange={(e) => setInputText(e.currentTarget.value)}
-                onKeyDown={(e) => onKeyDown(e)}
-                // onKeyUp={(e) =>
-                //   debounceGetSearch(e.target.value)
-                // }
-                autoFocus />
-              <Icon>
-                {
-                  inputText.length > 0 &&
-                  <Clear
-                    className="clear-search"
-                    onClick={() => (setInputText(""), setSearchUserList([]))}
-                  ></Clear>
-                }
-              </Icon>
-            </FlexBetween>
-          )}
-          <List className={searchUserList.length > 0 ? "search-box-container" : ""}>
-            {searchUserList &&
-              searchUserList?.map(
-                ({ _id, firstName, lastName, occupation, location, picturePath }) => {
-                  return (
-                    <>
-                      <button className="btn-list primary" key={_id + 1} onClick={() => routeChange(_id)}>
-                        <img src={('http://localhost:3001/assets/' + picturePath)} className="img-profile" />
-                        <ListItemText
-                          className="lst-items"
-                          primary={`${firstName} ${lastName}`}
-                          secondary={`${occupation}-${location}`}
-                        />
-                      </button>
-                      {(user._id !== _id) && <FriendRequest friendId={_id} />}
-                    </>
-                  );
-                }
-              )}
-          </List>
-        </div>
+        <SearchBar />
       </FlexBetween>
 
       {/* DESKTOP NAV */}
